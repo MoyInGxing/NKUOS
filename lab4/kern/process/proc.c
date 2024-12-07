cff2e36b84eb3cd7ee83aaaf347f26e52254e921
myx <2990238527@qq.com>
2024-12-07 08:53:23 +0000

    lab4

--- lab4/kern/process/proc.c
+++ lab4/kern/process/proc.c
@@ -86,7 +86,7 @@ static struct proc_struct *
 alloc_proc(void) {
     struct proc_struct *proc = kmalloc(sizeof(struct proc_struct));
     if (proc != NULL) {
-    //LAB4:EXERCISE1 YOUR CODE
+    //LAB4:2211462
     /*
      * below fields in proc_struct need to be initialized
      *       enum proc_state state;                      // Process state
@@ -102,7 +102,18 @@ alloc_proc(void) {
      *       uint32_t flags;                             // Process flag
      *       char name[PROC_NAME_LEN + 1];               // Process name
      */
-
+        proc->state = PROC_UNINIT;                           // 设置进程状态为未初始化
+        proc->pid = -1;                                      // 设置进程ID为-1（还未分配）
+        proc->cr3 = boot_cr3;                                // 设置CR3寄存器的值（页目录基址）
+        proc->runs = 0;                                      // 设置进程运行次数为0
+        proc->kstack = 0;                                    // 设置内核栈地址为0（还未分配）
+        proc->need_resched = 0;                              // 设置不需要重新调度
+        proc->parent = NULL;                                 // 设置父进程为空
+        proc->mm = NULL;                                     // 设置内存管理字段为空
+        memset(&(proc->context), 0, sizeof(struct context)); // 初始化上下文信息为0
+        proc->tf = NULL;                                     // 设置trapframe为空
+        proc->flags = 0;                                     // 设置进程标志为0
+        memset(proc->name, 0, PROC_NAME_LEN);                // 初始化进程名为0
 
     }
     return proc;
@@ -163,7 +174,7 @@ get_pid(void) {
 void
 proc_run(struct proc_struct *proc) {
     if (proc != current) {
-        // LAB4:EXERCISE3 YOUR CODE
+        // LAB4:2211462
         /*
         * Some Useful MACROs, Functions and DEFINEs, you can use them in below implementation.
         * MACROs or Functions:
@@ -172,6 +183,17 @@ proc_run(struct proc_struct *proc) {
         *   lcr3():                   Modify the value of CR3 register
         *   switch_to():              Context switching between two processes
         */
+       struct proc_struct *prev = current, *next = proc;
+       bool success = 0;
+       local_intr_save(success);
+       assert(success==1);
+         {
+              current = proc;
+              lcr3(proc->cr3);
+              switch_to(&(prev->context), &(next->context));
+         }
+        local_intr_restore(success);
+
        
     }
 }
@@ -273,7 +295,7 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
         goto fork_out;
     }
     ret = -E_NO_MEM;
-    //LAB4:EXERCISE2 YOUR CODE
+    //LAB4:2211462
     /*
      * Some Useful MACROs, Functions and DEFINEs, you can use them in below implementation.
      * MACROs or Functions:
@@ -298,9 +320,19 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
     //    5. insert proc_struct into hash_list && proc_list
     //    6. call wakeup_proc to make the new child process RUNNABLE
     //    7. set ret vaule using child proc's pid
-
-    
-
+    if ((proc = alloc_proc()) == NULL)
+        goto fork_out;
+    proc->parent = current;
+    if (setup_kstack(proc))
+        goto bad_fork_cleanup_kstack;
+    if (copy_mm(clone_flags, proc))
+        goto bad_fork_cleanup_proc;
+    copy_thread(proc, stack, tf);
+    proc->pid = get_pid();
+    hash_proc(proc);
+    list_add(&proc_list, &(proc->list_link));
+    wakeup_proc(proc);
+    ret = proc->pid;
 fork_out:
     return ret;
 
